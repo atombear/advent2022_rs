1b3361df30c0fe5f6230bfde96251f630c6f3916
alexander papageorge <alexander.papageorge@openlabsusa.org>
2022-12-02 00:43:16 -0500

    day 2

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+mod utils;
 mod problem20221201;
+mod problem20221202;
 
 fn main() {
     println!("{:?}", problem20221201::problem());
+    println!("{:?}", problem20221202::problem());
+
 }
---
+++ src/problem20221202.rs
@@ -0,0 +1,80 @@
+use std::path::PathBuf;
+
+use crate::utils::read_lines;
+
+
+// A - rock         1
+// B - paper        2
+// C - scissors     3
+
+// X - rock
+// Y - paper
+// Z - scissors
+
+fn get_score0(p0: char, p1: char) -> u64 {
+    match (p0, p1) {
+        ('A', 'X') => 1 + 3,
+        ('A', 'Y') => 2 + 6,
+        ('A', 'Z') => 3 + 0,
+        ('B', 'X') => 1 + 0,
+        ('B', 'Y') => 2 + 3,
+        ('B', 'Z') => 3 + 6,
+        ('C', 'X') => 1 + 6,
+        ('C', 'Y') => 2 + 0,
+        ('C', 'Z') => 3 + 3,
+        _ => panic!(),
+    }
+}
+
+
+// A - rock         1
+// B - paper        2
+// C - scissors     3
+
+// X - lose
+// Y - draw
+// Z - win
+
+fn get_score1(p0: char, p1: char) -> u64 {
+    match (p0, p1) {
+        ('A', 'X') => 0 + 3,
+        ('A', 'Y') => 3 + 1,
+        ('A', 'Z') => 6 + 2,
+        ('B', 'X') => 0 + 1,
+        ('B', 'Y') => 3 + 2,
+        ('B', 'Z') => 6 + 3,
+        ('C', 'X') => 0 + 2,
+        ('C', 'Y') => 3 + 3,
+        ('C', 'Z') => 6 + 1,
+        _ => panic!(),
+    }
+}
+
+
+pub fn problem() -> (u64, u64) {
+    let data_dir = env!("CARGO_MANIFEST_DIR").to_owned();
+    let data_path: PathBuf = [
+        data_dir,
+        "src".to_string(),
+        "input2.txt".to_string()
+    ].iter().collect();
+
+    let mut score0: u64 = 0;
+    let mut score1: u64 = 0;
+
+    if let Ok(lines) = read_lines(data_path) {
+        for line in lines {
+            if let Ok(num_str) = line {
+
+                let p0 = num_str.chars().nth(0).unwrap();
+                let p1 = num_str.chars().nth(2).unwrap();
+
+                score0 += get_score0(p0, p1);
+
+                score1 += get_score1(p0, p1);
+            }
+        }
+    }
+
+    return (score0, score1)
+}
