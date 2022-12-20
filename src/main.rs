df37a4fda222310aefe6baf8c3e03643f3324354
alexander papageorge <alexander.papageorge@openlabsusa.org>
2022-12-20 17:46:35 -0500

    day 20

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,7 @@ mod problem20221216;
 mod problem20221217;
 mod problem20221218;
 mod problem20221219;
+mod problem20221220;
 
 fn process_answer(answers: &mut Vec<String>, idx: usize, ans0: String, ans1: String) {
     while answers.len() <= idx {
@@ -51,6 +52,7 @@ fn main() {
         problem20221217::problem,
         problem20221218::problem,
         problem20221219::problem,
+        problem20221220::problem,
     ] {
         let (idx, ans0, ans1) = daily_fn();
         process_answer(&mut answers, idx, format!("{}", ans0), format!("{}", ans1));
